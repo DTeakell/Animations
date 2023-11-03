8b81c2a53c6535900991daabab0e8e0197439e11
Dillon Teakell <teakelld@icloud.com>
2023-11-02 21:38:52 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Dillon Teakell on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
